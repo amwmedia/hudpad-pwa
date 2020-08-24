9218d67dab0a54a1c1471d44056f34a4c5c2dd6f
Andrew Worcester <aworcester@frontlineed.com>
2020-08-24 09:11:16 -0400

    adds precache using workbox

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,7 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+import {precacheAndRoute} from 'workbox-precaching';
+
+precacheAndRoute(self.__WB_MANIFEST);
 
 if (workbox) {
   console.log(`Yay! Workbox is loaded 🎉`);
---
+++ workbox-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  "globDirectory": "dist/",
+  "globPatterns": [
+    "**/*.{html,js,png}"
+  ],
+  "swDest": "dist/sw.js",
+  "swSrc": "src/sw.js"
+};
