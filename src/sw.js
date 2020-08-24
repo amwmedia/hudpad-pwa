98e7467bc01ac250e6171143f274e78345c5d31e
Andrew Worcester <aworcester@frontlineed.com>
2020-08-24 09:55:47 -0400

    updates service worker so that it works with parcel

--- src/sw.js
+++ src/sw.js
@@ -1,7 +1,6 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
-// import {precacheAndRoute} from 'workbox-precaching';
 
-precacheAndRoute(self.__WB_MANIFEST);
+workbox.precacheAndRoute(self.__WB_MANIFEST);
 
 if (workbox) {
   console.log(`Yay! Workbox is loaded 🎉`);
